bb10455c5a6550313922a187da3c7cd7dae338a7
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-09-24 14:30:17 +0200

    Termine sortieren

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require_once("templates/header.php");
-$stmt = $pdo->prepare('SELECT * ,(SELECT source From blog_images WHERE blog_images.blog_entrys_id=blog_entrys.blog_entrys_id AND prev_img=1) AS source,(SELECT alt From blog_images WHERE blog_images.blog_entrys_id=blog_entrys.blog_entrys_id AND prev_img=1) AS alt FROM blog_entrys where visible = 1 ORDER BY created_at desc LIMIT 6;');
+$stmt = $pdo->prepare('SELECT * ,(SELECT source From blog_images WHERE blog_images.blog_entrys_id=blog_entrys.blog_entrys_id AND prev_img=1) AS source,(SELECT alt From blog_images WHERE blog_images.blog_entrys_id=blog_entrys.blog_entrys_id AND prev_img=1) AS alt FROM blog_entrys where visible = 1 ORDER BY created_at asc LIMIT 6;');
 $stmt->execute();
 $blogentrys = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $pdo->prepare('SELECT * FROM events where visible = 1 ORDER BY date desc LIMIT 6;');
+$stmt = $pdo->prepare('SELECT * FROM events where visible = 1 ORDER BY date asc LIMIT 6;');
 $stmt->execute();
 $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
