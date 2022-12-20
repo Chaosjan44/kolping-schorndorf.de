447a62ff1974e47908885b4a737849e108342253
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-12-20 19:41:42 +0100

    Updated Plausible domain

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@ session_start();
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
-    <script defer data-domain="kolping-schorndorf.de" src="https://plausible.schniebs.com/js/plausible.js"></script>
+    <script defer data-domain="kolping-schorndorf.de" src="https://plausible.schniebs.dev/js/script.js"></script>
     <link rel="stylesheet" href="/css/styles.min.css">      <!-- Link Stylesheet -->
     <link rel="stylesheet" href="/css/styles.css">          <!-- Link Stylesheet -->
     <link rel="stylesheet" href="/css/dark.css" disabled>   <!-- Link Dark Stylesheet and disable it -->
