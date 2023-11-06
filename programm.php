5edf59b76ca7790a30419423c6360b8dffa15d46
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2023-11-06 21:52:42 +0100

    added events

--- programm.php
+++ programm.php
@@ -11,13 +11,30 @@ echo $buffer;
 <div class="container py-3">
     <div style="min-height: 80vh;">
         <h1 class="display-3 text-center mb-3 text-kolping-orange">Unser Programm zum Herunterladen</h1>
-        <div class="row row-cols-1 gx-3">
+        <!-- <div class="row row-cols-1 gx-3">
             <div class="col p-2">
                 <div class="card cbg2 shadow1" style="height: 100% !important;">
                     <div class="card-body ctext">
                         <h3 class="card-title text-center">Programm 2023 März - Juli</h3>
                         <div class="justify-content-center text-center">
                             <form method="get" action="media/programm/2023_03-07.ics">
+                                <button class="btn btn-kolping mx-auto my-2" type="submit">
+                                    <span class="card-text">Herunterladen</span>
+                                    <i class="bi bi-download"></i>
+                                </button>
+                            </form> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="row row-cols-1 gx-3">
+            <div class="col p-2">
+                <div class="card cbg2 shadow1" style="height: 100% !important;">
+                    <div class="card-body ctext">
+                        <h3 class="card-title text-center">Programm 2023 August - Januar</h3>
+                        <div class="justify-content-center text-center">
+                            <form method="get" action="media/programm/2023_08-01.ics">
                                 <button class="btn btn-kolping mx-auto my-2" type="submit">
                                     <span class="card-text">Herunterladen</span>
                                     <i class="bi bi-download"></i>
